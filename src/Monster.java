2c5f65b1b6ede3fedefab5fc1b457279e463d22a
= <=>
2017-10-02 10:38:59 -0400

    initial

---
+++ src/Monster.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+
+/**
+ * @author Admin
+ *
+ */
+public class Monster
+{
+	
+	public int					health;
+	public int					armor;
+	public int					damage;
+	public ArrayList<Monster>	monsterAL;
+}
