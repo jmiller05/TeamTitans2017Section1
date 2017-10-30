ae719da73fb5fd129e6616b4a1dc7cdd7261e46f
jmiller05 <jmiller13@ggc.edu>
2017-10-30 09:39:55 -0400

    formatting
    formatting

--- src/Puzzle.java
+++ src/Puzzle.java
@@ -1,4 +1,4 @@
- 	import java.util.ArrayList;
+import java.util.ArrayList;
 
 public class Puzzle {
 	private String puzzleID;
@@ -6,47 +6,47 @@ public class Puzzle {
 	private String puzzleDescription;
 	private String artifactID;
 	private String solution;
-    	private Room location;
-  	private String hint;
-  	private ArrayList<Item> requiredItems = new ArrayList<Item>();
- 	
- 	public Puzzle(String ID, String name, String desc,String art, String solu)
- 	{
- 		this.puzzleID = ID;
-	this.puzzleName = name;
-	this.puzzleDescription = desc;
- 	this.artifactID = art;
- 		this.solution = solu;
- 	}
- 	public void getPuzzleName()
- 	{
- 		System.out.println(this.puzzleName);
- 	}
- 	public void getPuzzleID()
- 	{
- 		System.out.println(this.puzzleID);
- 	}
- 	
- 	public void gePuzzleDescription()
- 	{
+	private Room location;
+	private String hint;
+	private ArrayList<Item> requiredItems = new ArrayList<Item>();
+	
+	public Puzzle(String ID, String name, String desc,String art, String solu)
+	{
+		this.puzzleID = ID;
+		this.puzzleName = name;
+		this.puzzleDescription = desc;
+		this.artifactID = art;
+		this.solution = solu;
+	}
+	public void getPuzzleName()
+	{
+		System.out.println(this.puzzleName);
+	}
+	public void getPuzzleID()
+	{
+		System.out.println(this.puzzleID);
+	}
+	
+	public void gePuzzleDescription()
+	{
 		System.out.println(this.puzzleDescription);
 	}
- 	public void getArtifactID()
- 	{ 
- 		System.out.println(this.artifactID);
- 	}
- 	public void getSolution()
- 	{
- 		System.out.println(this.solution);
- 	}
- 	public void addItem(Item item)
- 	{		
- 		requiredItems.add(item);
- 	}
- 	public void requestHint()
- 	{
- 		
- 	}
+	public void getArtifactID()
+	{ 
+		System.out.println(this.artifactID);
+	}
+	public void getSolution()
+	{
+		System.out.println(this.solution);
+	}
+	public void addItem(Item item)
+	{		
+		requiredItems.add(item);
+	}
+	public void requestHint()
+	{
+		
+	}
 	
- 	
-  }
+	
+}
