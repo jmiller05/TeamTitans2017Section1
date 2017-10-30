469677743911a0b6a69b770be78ca301f763006c
elamkie <elamkie@ggc.edu>
2017-10-30 10:20:47 -0400

    Implemented EventHandler<ActionEvent> in Controller class

--- src/Controller.java
+++ src/Controller.java
@@ -1,5 +1,20 @@
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 
-public class Controller
+public class Controller implements EventHandler<ActionEvent>
 {
-	
+
+	/* (non-Javadoc)
+	 * @see javafx.event.EventHandler#handle(javafx.event.Event)
+	 */
+	@Override
+	public void handle(ActionEvent event)
+	{
+		// TODO Auto-generated method stub
+		
+	}
+	 
 }
+
+
+
