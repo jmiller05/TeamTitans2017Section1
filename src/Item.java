32a9889612326a20b527f5edf50de9579be87edf
bmashlum <bmashlum@gmail.com>
2017-11-01 10:35:23 -0400

    workin
    adding inheritance, constructors ect... small work. Making sure I'm seen in git

--- src/Item.java
+++ src/Item.java
@@ -1,11 +1,67 @@
 import java.util.ArrayList;
 
-public class Item {
+public class Item extends Entity {
 
-	protected int itemID;
-	protected String itemName;
-	protected String itemDescription;
-	protected ArrayList<Room> locations = new ArrayList<Room>();
-	protected Monster droppedBy;
+	private int itemID;
+	private String itemName;
+	private String itemDescription;
+	private ArrayList<Room> locations = new ArrayList<Room>();
+	private ArrayList<Item> items = new ArrayList<Item>();
+	private Monster droppedBy;
 
+	public Item(int itemID, String itemName, String itemDescription, Monster droppedBy) {
+		this.itemID = itemID;
+		this.itemName = itemName;
+		this.itemDescription = itemDescription;
+	}
+
+	/**
+	 * @return the itemID
+	 */
+	public int getItemID()
+	{
+		return itemID;
+	}
+
+	/**
+	 * @return the itemName
+	 */
+	public String getItemName()
+	{
+		return itemName;
+	}
+
+	/**
+	 * @return the itemDescription
+	 */
+	public String getItemDescription()
+	{
+		return itemDescription;
+	}
+
+	/**
+	 * @return the locations
+	 */
+	public ArrayList<Room> getLocations()
+	{
+		return locations;
+	}
+
+	/**
+	 * @return the items
+	 */
+	public ArrayList<Item> getItems()
+	{
+		return items;
+	}
+
+	/**
+	 * @return the droppedBy
+	 */
+	public Monster getDroppedBy()
+	{
+		return droppedBy;
+	}
+	
+	
 }
