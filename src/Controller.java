64076d82f6bf55ca957770590f3bbf955bebec56
jmiller05 <jmiller13@ggc.edu>
2017-11-22 17:22:53 -0500

    added Monsters
    populate monsters and added examine monster button

--- src/Controller.java
+++ src/Controller.java
@@ -193,6 +193,12 @@ public class Controller
 		inventoryView.setItems(inventoryList);
 		inventoryStage.show();	
 	}
+	
+	@FXML
+	private void examineMonster(ActionEvent event)
+	{
+		
+	}
 
 	private void checkValidExits()
 	{
