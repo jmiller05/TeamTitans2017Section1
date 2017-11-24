670f37fa8f460b2b59a110686af4749d5327441e
elamkie <elamkie@ggc.edu>
2017-11-24 16:59:52 -0500

    Modified Item

--- src/Item.java
+++ src/Item.java
@@ -14,92 +14,47 @@ import java.util.HashMap;
 
 public class Item implements Serializable{
 	
-	protected String itemID;
+	static final long serialVersionUID = 300;
+	
+	protected int itemID;
 	protected String itemName;
 	protected String itemDescription;
-	private ArrayList<Room> location = new ArrayList<Room>();
-	protected Monster droppedBy;
-	protected Room foundIn;
 	
-	public Item(String itemID, String itemName, String itemDescription, Monster droppedBy) {//need to add room location where item found
+	public Item(int itemID, String itemName, String itemDescription)
+	{
 		this.itemID = itemID;
 		this.itemName = itemName;
 		this.itemDescription = itemDescription;
-		this.droppedBy = droppedBy;
 	}
-	
-	
-	
-	/**
-	 * @return the foundIn
-	 */
-	/**public Room getFoundIn()
-	{
-		return foundIn;
-	}
-	 */
-	/**
-	 * @param foundIn the foundIn to set
-	 */
-	/**public void setFoundIn(Room foundIn)
-	{
-		this.foundIn = foundIn;
-	}*/
-	
-	public void dropItem()
-	{
-		
-	}
-	
-	public void collectItem()
-	{
-		
-	}
-	
-	/**
-	 * @return the itemID
-	 */
-	public String getItemID()
+
+	public int getItemID()
 	{
 		return itemID;
 	}
-	
-	/**
-	 * @return the itemName
-	 */
+
+	public void setItemID(int itemID)
+	{
+		this.itemID = itemID;
+	}
+
 	public String getItemName()
 	{
 		return itemName;
 	}
-	
-	/**
-	 * @return the itemDescription
-	 */
+
+	public void setItemName(String itemName)
+	{
+		this.itemName = itemName;
+	}
+
 	public String getItemDescription()
 	{
 		return itemDescription;
 	}
-	
-	public void addLocation(Room room)
+
+	public void setItemDescription(String itemDescription)
 	{
-		location.add(room);
-	}
-	
-	/**
-	 * @return the locations
-	 */
-	public int getLocation()
-	{
-		return location.get(0).getRoomID();		
-	}
-	
-	
-	/**
-	 * @return the droppedBy
-	 */
-	public  Monster getDroppedBy()
-	{
-		return droppedBy;
+		this.itemDescription = itemDescription;
 	}
 	
 	public static HashMap<Integer, Item> loadItemsInRooms(String filename)
