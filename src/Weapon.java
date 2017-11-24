fdef5c02ffee8dd020f83a2ae8be14ba91603525
elamkie <elamkie@ggc.edu>
2017-11-24 17:01:12 -0500

    Modified Weapon

--- src/Weapon.java
+++ src/Weapon.java
@@ -6,39 +6,29 @@
 public class Weapon extends Item {
 
 	
-	private int damage;
-	
-	
-	/**
-	 * @param itemID
-	 * @param itemName
-	 * @param itemDescription
-	 * @param droppedBy
-	 */
-	public Weapon(String itemID, String itemName, String itemDescription, int damage, Monster droppedBy)
+	protected int damage;
+
+	public Weapon(int itemID, String itemName, String itemDescription)
 	{
-		super(itemID, itemName, itemDescription, droppedBy);
+		super(itemID, itemName, itemDescription);
+		damage = 1;
+	}
+	
+	public Weapon(int itemID, String itemName, String itemDescription, int damage)
+	{
+		super(itemID, itemName, itemDescription);
 		this.damage = damage;
 	}
 
-
-	/**
-	 * @return the damage
-	 */
 	public int getDamage()
 	{
 		return damage;
 	}
 
-
-	/**
-	 * @param damage the damage to set
-	 */
 	public void setDamage(int damage)
 	{
 		this.damage = damage;
 	}
-
 	
 
 }
