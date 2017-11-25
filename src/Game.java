deba19fd5d15ac8672fd2067f2a66da9156a4555
Joshua Austin <jaustin1@ggc.edu>
2017-11-25 03:56:14 -0500

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -25,6 +25,7 @@ public class Game extends Application
 	private ArrayList<String>				roomNameAL;										
 	private ArrayList<String>				roomDescriptionAL;								
 	private ArrayList<Monster> monsterArray = new ArrayList<Monster>();
+	private ArrayList<Puzzle> puzzleArray = new ArrayList<Puzzle>();
 	public static HashMap<Integer, Item>	itemsInRooms	= new HashMap<Integer, Item>();
 	
 	public Player							gamePlayer;
@@ -41,6 +42,7 @@ public class Game extends Application
 		FXMLLoader loader = new FXMLLoader();
 		FXMLLoader inventoryLoader = new FXMLLoader();
 		FXMLLoader monsterEncounterLoader = new FXMLLoader();
+		FXMLLoader PuzzleLoader = new FXMLLoader();
 		
 		gamePlayer = new Player(10, 10, 1);
 		
@@ -70,6 +72,8 @@ public class Game extends Application
 		addMonsterRooms();
 		//populateRoomItems();
 		saveItemsInRooms();
+		populatePuzzles();
+		addPuzzleRooms();
 		
 		//
 		// try{
@@ -88,9 +92,11 @@ public class Game extends Application
 		loader.setController(gameController);
 		inventoryLoader.setController(gameController);
 		monsterEncounterLoader.setController(gameController);
+		PuzzleLoader.setController(gameController);
 		loader.setLocation(getClass().getResource("View.fxml"));
 		inventoryLoader.setLocation(getClass().getResource("Inventory.fxml"));
 		monsterEncounterLoader.setLocation(getClass().getResource("MonsterEncounter.fxml"));
+		PuzzleLoader.setLocation(getClass().getResource("Puzzle.fxml"));
 		Parent root = loader.load();
 		Parent inventory = inventoryLoader.load();
 		Parent encounter = monsterEncounterLoader.load();
@@ -418,4 +424,29 @@ public class Game extends Application
 		
 	} 
 	
+	public void populatePuzzles()
+	 	{
+	 		Puzzle p0 = new Puzzle("PZ00", "Torch", "Pick the torch in room 4 in order to enter and navigate room 6", "Collect and equip torch in room 4 to illuminate room 6");
+	 		Puzzle p1 = new Puzzle("PZ01", "Runes", "There will be two runes that is needed to be slotted into the door (Dr_18) in Room 15 in order to enter Room 18", "The Runes from room 16 and room 17 will unlock the door in Room 15 to enter Room 18");
+	 		Puzzle p2 = new Puzzle("PZ02", "Skull Pedestal", "Find 3 golden skulls and assemble them in order to attract the final monster - One golden skull per floor", "Assemble all 3 skulls in room 29 from the skulls from Rooms 9, 22, 26");
+	 		Puzzle p3 = new Puzzle("PZ03", "Spider Webs", "Destroy 3 spider webs - One room will be unlocked, one spider web will drop the key to room 8", "Destroy spider webs until the key to room 8 drops from one of the webs");
+	 		Puzzle p4 = new Puzzle("PZ04", "Centaur", "Kill the centaur and collect the key in order to access Room 21", "Kill centaur and collect key");
+	 		Puzzle p5 = new Puzzle("PZ05", "Witch craft", "Kill witch, drops the key to room 22", "Collect key from witchâ€™s decomposing body");
+	 		Puzzle p6 = new Puzzle("PZ06", "Torch puzzle", "There should be 3 torches that have to be lit in order to unlock the door to room 29", "The three unlit torches in Room 24 need to be lit in order to enter room 29 1-0-1 1 = Lit 0 = Off");
+	 		
+	 		puzzleArray.add(p0); puzzleArray.add(p1); puzzleArray.add(p2); puzzleArray.add(p3); 
+	 		puzzleArray.add(p4); puzzleArray.add(p5); puzzleArray.add(p6);
+	 	}
+	
+	public void addPuzzleRooms()
+		{
+	 		puzzleArray.get(0).addLocation(roomAL.get(4));
+	 		puzzleArray.get(1).addLocation(roomAL.get(15));
+	 		puzzleArray.get(2).addLocation(roomAL.get(29));
+	 		puzzleArray.get(3).addLocation(roomAL.get(5));
+	 		puzzleArray.get(4).addLocation(roomAL.get(18));
+	 		puzzleArray.get(5).addLocation(roomAL.get(20));
+	 		puzzleArray.get(6).addLocation(roomAL.get(24));
+	 		
+	 	}	
 }
